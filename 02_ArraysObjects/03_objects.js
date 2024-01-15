9b9f6b2fcd6bc1af96a4db29d6bbbacc919342a2
Ashief <07ashief@gmail.com.com>
2024-01-15 14:08:46 +0530

    Object Notes add on

--- 02_ArraysObjects/03_objects.js
+++ 02_ArraysObjects/03_objects.js
@@ -1,11 +1,19 @@
-// singleton
-// Object.create
+// Object Definition:
 
-// object literals
+// An object is a collection of key-value pairs where each key is a unique string or symbol (property) and each value can be of any data type, including other objects, functions, or primitive types.
 
-const mySym = Symbol("key1")
+// Singleton:
+// A singleton is an object that can only be instantiated once. In JavaScript, object literals are often used to create singleton objects.
 
+// Object.create:
+// Object.create is a method that allows the creation of a new object with a specified prototype object. It is a way to create objects and define their properties in a more dynamic manner.
 
+// Object Literals:
+// Object literals are a convenient way to create objects in JavaScript by providing a concise syntax for defining properties and their values within curly braces.
+
+const mySym = Symbol("key1");
+
+// Creating an Object Literal
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
@@ -15,24 +23,41 @@ const JsUser = {
     email: "hitesh@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
-}
+};
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+console.log(typeof JsUser);
+// Outputs: object
+// Checking the type of 'JsUser' using the 'typeof' operator.
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+console.log(JsUser instanceof Object);
+// Outputs: true
+// Checking if 'JsUser' is an instance of the Object constructor.
 
+// Modifying Object Properties
+JsUser.email = "hitesh@chatgpt.com";
+// Changing the value of the 'email' property.
+
+Object.freeze(JsUser);
+// Freezing the 'JsUser' object to make it immutable.
+
+JsUser.email = "hitesh@microsoft.com";
+// Attempting to change the 'email' property after freezing.
+
+console.log(JsUser);
+// Outputs: Object is frozen, changes are not allowed
+
+// Adding Methods to Objects
 JsUser.greeting = function(){
     console.log("Hello JS user");
-}
+};
+// Adding a method 'greeting' to the 'JsUser' object.
+
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
-}
+};
+// Adding another method 'greetingTwo' that references a property of the object.
 
 console.log(JsUser.greeting());
+// Outputs: Hello JS user
 console.log(JsUser.greetingTwo());
+// Outputs: Hello JS user, Hitesh
--- 02_ArraysObjects/04_objects.js
+++ 02_ArraysObjects/04_objects.js
@@ -1,12 +1,18 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// Object Creation and Manipulation
 
-tinderUser.id = "123abc"
-tinderUser.name = "Sammy"
-tinderUser.isLoggedIn = false
+// Creating an Object using Object Literal:
+const tinderUser = {};
+// An empty object created using object literal notation.
 
-// console.log(tinderUser);
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+// Adding properties to the 'tinderUser' object.
 
+console.log(tinderUser);
+// Outputs: { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+// Nested Object:
 const regularUser = {
     email: "some@gmail.com",
     fullname: {
@@ -15,67 +21,67 @@ const regularUser = {
             lastname: "choudhary"
         }
     }
-}
+};
 
-// console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname.userfullname.firstname);
+// Outputs: hitesh
+// Accessing nested properties within the 'regularUser' object.
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-const obj4 = {5: "a", 6: "b"}
+// Merging Objects:
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+const obj4 = { 5: "a", 6: "b" };
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj3 = {...obj1, ...obj2};
+// Merging objects using the spread operator.
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+console.log(obj3);
+// Outputs: { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
-
+// Array of Objects:
 const users = [
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-]
+    { id: 1, email: "h@gmail.com" },
+    { id: 1, email: "h@gmail.com" },
+    { id: 1, email: "h@gmail.com" }
+];
 
-users[1].email
-// console.log(tinderUser);
+console.log(users[1].email);
+// Outputs: h@gmail.com
+// Accessing properties within an array of objects.
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// Object Methods:
+console.log(Object.keys(tinderUser));
+// Outputs: [ 'id', 'name', 'isLoggedIn' ]
+// Getting an array of keys from the 'tinderUser' object.
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(Object.values(tinderUser));
+// Outputs: [ '123abc', 'Sammy', false ]
+// Getting an array of values from the 'tinderUser' object.
 
+console.log(Object.entries(tinderUser));
+// Outputs: [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+// Getting an array of key-value pairs from the 'tinderUser' object.
 
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// Outputs: true
+// Checking if the 'tinderUser' object has the 'isLoggedIn' property.
+
+// Destructuring:
 const course = {
     coursename: "js in hindi",
     price: "999",
     courseInstructor: "hitesh"
-}
+};
 
-// course.courseInstructor
+const {courseInstructor: instructor} = course;
+// Destructuring the 'course' object to get the value of 'courseInstructor'.
 
-const {courseInstructor: instructor} = course
-
-// console.log(courseInstructor);
 console.log(instructor);
+// Outputs: hitesh
+// Accessing the destructured value.
 
-// {
-//     "name": "hitesh",
-//     "coursename": "js in hindi",
-//     "price": "free"
-// }
-
-[
-    {},
-    {},
-    {}
-]
+// Notes:
+// - Object literals provide a convenient way to create and initialize objects.
+// - Merging objects can be done using spread operators or Object.assign().
+// - Destructuring allows extracting values from objects easily.
+// - Object methods like Object.keys, Object.values, and Object.entries help in working with object properties.
